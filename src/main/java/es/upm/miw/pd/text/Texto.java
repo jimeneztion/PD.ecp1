3b39458f6de8ef2627a23992618ac1eb3da09b81
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2015-10-16 09:31:33 +0200

    Se añade el patron text

---
+++ src/main/java/es/upm/miw/pd/text/Texto.java
@@ -0,0 +1,15 @@
+package es.upm.miw.pd.text;
+
+public class Texto extends Componente {
+
+    @Override
+    public void add(Componente h) {
+
+    }
+
+    @Override
+    public String dibujar(boolean b) {
+        return null;
+    }
+
+}
