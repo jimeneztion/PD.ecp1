e31bcda8af898ef7de5eea239cc2b816374082b9
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2015-10-17 18:20:54 +0200

    Se añade el patron visitor figure

---
+++ src/main/java/es/upm/miw/pd/command/calculator/AddCommand.java
@@ -0,0 +1,22 @@
+package es.upm.miw.pd.command.calculator;
+
+import upm.jbb.IO;
+
+public class AddCommand extends Comando {
+    
+    private Calculator cal; 
+
+    public AddCommand(Calculator cal) {
+       this.cal = cal;
+    }
+
+    public String name() {
+        return "Sumar";
+    }
+
+    public void execute() {
+        int i = IO.getIO().readInt();
+        cal.add(i);
+    }
+
+}
