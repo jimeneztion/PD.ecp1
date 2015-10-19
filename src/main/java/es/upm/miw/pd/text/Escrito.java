f2e1b19439677a4cb81d647623df0d4ffdfe3131
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2015-10-19 15:48:41 +0200

    Se añade la clase escrito representa un listado de componentes

---
+++ src/main/java/es/upm/miw/pd/text/Escrito.java
@@ -0,0 +1,17 @@
+package es.upm.miw.pd.text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Escrito extends Componente {
+
+    public List<Componente> listado;
+
+    public Escrito() {
+        super();
+        this.listado = new ArrayList<Componente>() ;
+    }
+    
+    
+
+}
