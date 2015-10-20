4219052fc1e94082884de20f0840cc8a83a5683b
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2015-10-20 22:46:52 +0200

    Se añade MementoCalculadora

---
+++ src/main/java/es/upm/miw/pd/command/calculator/memento/MementoCalculadora.java
@@ -0,0 +1,16 @@
+package es.upm.miw.pd.command.calculator.memento;
+
+public class MementoCalculadora {
+
+    private int valor;
+
+    public MementoCalculadora(int valor) {
+        super();
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+}
